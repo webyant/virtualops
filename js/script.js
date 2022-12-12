a0dbeb7f023b1b3a080d0a1e84e5a26a31897de6
nagjisankhala <115863097+nagjisankhala@users.noreply.github.com>
2022-12-12 17:47:01 +0530

    changes

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
 $(document).ready(function() {
-    // $(window).scroll(function() {
-    //     var height = $(window).scrollTop();
-    //     if (height > 300) {
-    //         $(".site-header").addClass("headerup animate__animated animate__fadeInDown");
-    //     } else {
-    //         $(".site-header").removeClass("headerup animate__animated animate__fadeInDown");
-    //     }
-    // });
+    $(window).scroll(function() {
+        var height = $(window).scrollTop();
+        if (height > 300) {
+            $(".site-header").addClass("headerup animate__animated animate__fadeInDown");
+        } else {
+            $(".site-header").removeClass("headerup animate__animated animate__fadeInDown");
+        }
+    });
     new WOW().init();
 
     $('.slider').slick({
@@ -15,5 +15,25 @@ $(document).ready(function() {
         speed: 300,
         slidesToShow: 1,
         adaptiveHeight: true
+    });
+    $('.marketing__slider').slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        arrows: false,
+        pauseOnHover: false
+    });
+    $(function() {
+        var title = $(".accordian__block h3");
+        title
+            .on("click", function() {
+                var ThisIt = $(this);
+                $(".accordian__block h3").not(ThisIt).removeClass("active");
+                $(this).toggleClass("active");
+                $(".accordian__content").not(ThisIt.next(".accordian__content")).slideUp(400);
+                ThisIt.next(".accordian__content").slideToggle(400);
+            })
     });
 });
