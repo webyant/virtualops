413b91f4e22a9270beb369c0656859dc669505e7
dhavalsankhala55 <sankhaladhaval9@gmail.com>
2023-01-02 11:56:08 +0530

    wp change

--- wp-content/themes/virtualops/front-page.php
+++ wp-content/themes/virtualops/front-page.php
@@ -101,74 +101,97 @@ get_header();
     <section class="section section--succeed pb-0">
         <div class="wrap">
             <div class="succeed__heading text-center">
-                <h2 class="text-orange dots__title">
-                    <span>Collaborate</span><span>Optimize</span><span>Accelerate</span>
-                </h2>
-                <p class="font-md">Stay Ahead of the Competition </p>
+                <?php if( get_field('succeed_heading') ): ?>
+                    <h2 class="text-orange dots__title" data-wow-duration="0.5s"><?php the_field('succeed_heading'); ?></h2>
+                <?php endif; ?>
+                <?php if( get_field('succeed_description') ): ?>
+                    <p class="font-md"><?php the_field('succeed_description'); ?></p>
+                <?php endif; ?>
             </div>
             <div class="row mt-32">
                 <div class="col-6 col-md-12">
                     <div class="marketing__img">
-                        <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/phone-frame.png"
-                            alt="phone">
+                        <?php  if( get_field('phone_frame_img') ) { ?>
+                            <?php echo wp_get_attachment_image( get_field('phone_frame_img'), 'full' );  ?>
+                        <?php } ?>
                         <div class="marketing__slider">
                             <div class="marketing__images">
-                                <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/marketing-slider.jpg"
-                                    alt="marketing-slider">
+                                <?php  if( get_field('marketing_slider') ) { ?>
+                                    <?php echo wp_get_attachment_image( get_field('marketing_slider'), 'full' );  ?>
+                                <?php } ?>
                             </div>
                             <div class="marketing__images">
-                                <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/marketing-slider-1.jpg"
-                                    alt="marketing-slider">
+                                <?php  if( get_field('marketing_slider1') ) { ?>
+                                    <?php echo wp_get_attachment_image( get_field('marketing_slider1'), 'full' );  ?>
+                                <?php } ?>
                             </div>
                             <div class="marketing__images">
-                                <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/marketing-slider-2.jpg"
-                                    alt="marketing-slider">
+                                <?php  if( get_field('marketing_slider2') ) { ?>
+                                    <?php echo wp_get_attachment_image( get_field('marketing_slider2'), 'full' );  ?>
+                                <?php } ?>
                             </div>
                             <div class="marketing__images">
-                                <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/marketing-slider-3.jpg"
-                                    alt="marketing-slider">
+                                <?php  if( get_field('marketing_slider3') ) { ?>
+                                    <?php echo wp_get_attachment_image( get_field('marketing_slider3'), 'full' );  ?>
+                                <?php } ?>
                             </div>
                             <div class="marketing__images">
-                                <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/marketing-slider-4.jpg"
-                                    alt="marketing-slider">
+                                <?php  if( get_field('marketing_slider4') ) { ?>
+                                    <?php echo wp_get_attachment_image( get_field('marketing_slider4'), 'full' );  ?>
+                                <?php } ?>
                             </div>
                             <div class="marketing__images">
-                                <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/marketing-slider-5.jpg"
-                                    alt="marketing-slider">
+                                <?php  if( get_field('marketing_slider5') ) { ?>
+                                    <?php echo wp_get_attachment_image( get_field('marketing_slider5'), 'full' );  ?>
+                                <?php } ?>
                             </div>
                             <div class="marketing__images">
-                                <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/marketing-slider-6.jpg"
-                                    alt="marketing-slider">
+                                <?php  if( get_field('marketing_slider6') ) { ?>
+                                    <?php echo wp_get_attachment_image( get_field('marketing_slider6'), 'full' );  ?>
+                                <?php } ?>
                             </div>
                             <div class="marketing__images">
-                                <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/marketing-slider-7.jpg"
-                                    alt="marketing-slider">
+                                <?php  if( get_field('marketing_slider7') ) { ?>
+                                    <?php echo wp_get_attachment_image( get_field('marketing_slider7'), 'full' );  ?>
+                                <?php } ?>
                             </div>
                         </div>
                     </div>
                 </div>
                 <div class="col-6 col-md-12">
-                    <h3 class="mb-40">Benefits of High Quality Product Data</h3>
+                    <?php if( get_field('succeed_right_heading') ): ?>
+                        <h3 class="mb-40" data-wow-duration="0.5s"><?php the_field('succeed_right_heading'); ?></h3>
+                    <?php endif; ?>
                     <div class="succeed--content mt-24">
                         <div class="content__img wow bounceInUp">
-                            <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/line.jpg"
-                                alt="line">
+                            <?php  if( get_field('line_img') ) { ?>
+                                <?php echo wp_get_attachment_image( get_field('line_img'), 'full' );  ?>
+                            <?php } ?>
                         </div>
                         <div class="content--info">
                             <div class="info__text wow bounceInUp" data-wow-duration="1s">
-                                <h4> Improve Conversions </h4>
-                                <p>Accurate, enhanced and optimized product data makes searching and purchasing easier
-                                    and increases revenue</p>
+                                <?php if( get_field('improve_heading') ): ?>
+                                    <h4><?php the_field('improve_heading'); ?></h4>
+                                <?php endif; ?>
+                                <?php if( get_field('improve_content') ): ?>
+                                    <p><?php the_field('improve_content'); ?></p>
+                                <?php endif; ?>
                             </div>
                             <div class="info__text wow bounceInUp" data-wow-duration="1.5s">
-                                <h4>Reduce Bounce Rates</h4>
-                                <p>Consistent, clear product data and clean, high quality digital assets improve user
-                                    experience and reduces cart abandonment</p>
+                                <?php if( get_field('reduce_heading') ): ?>
+                                    <h4><?php the_field('reduce_heading'); ?></h4>
+                                <?php endif; ?>
+                                <?php if( get_field('reduce_content') ): ?>
+                                    <p><?php the_field('reduce_content'); ?></p>
+                                <?php endif; ?>
                             </div>
                             <div class="info__text wow bounceInUp" data-wow-duration="2s">
-                                <h4>Enhance Customer Loyalty </h4>
-                                <p>High quality product data and digital assets lead to customer delight and repeat
-                                    orders</p>
+                                <?php if( get_field('enhance_heading') ): ?>
+                                    <h4><?php the_field('enhance_heading'); ?></h4>
+                                <?php endif; ?>
+                                <?php if( get_field('enhance_content') ): ?>
+                                    <p><?php the_field('enhance_content'); ?></p>
+                                <?php endif; ?>
                             </div>
                         </div>
                     </div>
@@ -252,90 +275,30 @@ get_header();
     </section>
     <section class="section section--advertising">
         <div class="wrap">
-            <h2 class="mb-48 wow bounceInUp text-orange">Testimonials</h2>
+            <?php if( get_field('testimonials_heading') ): ?>
+                <h2 class="mb-48 wow bounceInUp text-orange"><?php the_field('testimonials_heading'); ?></h2>
+            <?php endif; ?>
+            <?php if( have_rows('testimonials_info') ): ?>
             <div class="slider">
+                <?php while( have_rows('testimonials_info') ): the_row(); ?>
                 <div class="advertising__slider">
                     <blockquote>
-                        <p>I was tasked to identify an outsourced partner that is knowledgeable, smart, flexible and
-                            competitive. After choosing Virtual Ops over 7 years back, I was impressed with the quick
-                            learning of Virtual Ops team, the diligence
-                            and systematic processes they established in short time that ensured timely deliverables
-                            with high quality. I appreciate their consultative efforts that were focused on improving
-                            our business outcomes. What I like most
-                            about them is their prompt service and flexibility to learn and implement new projects
-                            quickly. Other benefits that I get are competitive rates and high quality. Apart from being
-                            very collaborative and constructive, the
-                            team at Virtual Ops understands what they are doing. Over the past 7 years our relationship
-                            with Virtual Ops has become a real partnership. This allows us to explore new opportunities
-                            to support the development of our business.
-                        </p>
+                        <p><?php the_sub_field('testimonials_content'); ?></p>
                     </blockquote>
                     <div class="testimonial-title">
-                        <h3>Harry Drummer</h3>
-                        <p>COO – Adorama.com</p>
-                        <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/adorama.png"
-                            alt="adorama">
+                        <h3><?php the_sub_field('testimonials_title'); ?></h3>
+                        <p><?php the_sub_field('testimonials_text'); ?></p>
+                        <?php echo wp_get_attachment_image( get_sub_field('testimonials_img'), 'full' ); ?>
                     </div>
                 </div>
-                <div class="advertising__slider">
-                    <blockquote>
-                        <p>I have been working with Virtual Ops and Samir for almost 10 years. Samir has been a great
-                            partner to help us with our product content efforts. From day one of our engagement Samir
-                            has personally invested his time to understand
-                            our needs and developed the corrected processes to make sure that the job gets done in a
-                            very efficient and expedited manner. He has done the same for every new project that has
-                            come, he hands it off to his team only after
-                            he understands what’s needed to be done and has discussed and aligned with us on the process
-                            and the expected results.</p>
-                    </blockquote>
-                    <div class="testimonial-title">
-                        <h3>Bill Parnes</h3>
-                        <p>VP – Scuba.com</p>
-                        <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/scuba.png"
-                            alt="scuba">
-                    </div>
-                </div>
-                <div class="advertising__slider">
-                    <blockquote>
-                        <p>Virtual Ops has been our partner for our product content requirements for over 6 years now.
-                            They have consistently delivered quality for all our product data requirements including
-                            migration to Product Information Management
-                            system. What I like about Samir and his team is that they constantly learn, develop
-                            knowledgebase, use tools and technologies that helps deliver high quality. They are always
-                            flexible and available to work on urgent and
-                            new requirements at short notice. They are truly our trusted partner. Always reliable and
-                            fast, even after I leave for the day, I can always count on you guys to get requests done.
-                            Thank you!</p>
-                    </blockquote>
-                    <div class="testimonial-title">
-                        <h3>Steve Anderson</h3>
-                        <p>Web content – Adorama</p>
-                        <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/adorama.png"
-                            alt="adorama">
-                    </div>
-                </div>
-                <div class="advertising__slider">
-                    <blockquote>
-                        <p>It has been a pleasure to work with Virtual Ops. I co-ordinate and manage multiple partners
-                            for our Product Information Management platform updates, and Virtual Ops clearly stands out
-                            in their deliverables, communication, promptness
-                            and quality. Not only have they consistently delivered high quality on time, but they go
-                            above and beyond to provide constructive feedback on process improvements, correction of
-                            errors from SKU sources, removing duplication/errors.
-                            Samir’s communication be it on JIRA or emails has been clear and objective. I recommend
-                            anyone to partner with Virtual Ops for their product data management requirements.
-                        </p>
-                    </blockquote>
-                    <div class="testimonial-title">
-                        <h3>Jean-Alex Ervilus</h3>
-                        <p>Merchandise Specialist - Sunnysports.com</p>
-                        <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/sunnysports.png"
-                            alt="Sunnysports">
-                    </div>
-                </div>
+                <?php endwhile; ?>
             </div>
+            <?php endif; ?>    
             <img class="doots" src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/dot-bg.png"
                 alt="Doots">
+            <?php  if( get_field('doots') ) { ?>
+                    <?php echo wp_get_attachment_image( get_field('doots'), 'full' );  ?>
+            <?php } ?>
         </div>
     </section>
 
