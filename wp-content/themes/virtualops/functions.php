8f46bb20fc61fedfb404a9fc5f6d680770234b30
WP Developer <developer@example.com>
2023-01-04 08:13:41 +0530

    Changes

--- wp-content/themes/virtualops/functions.php
+++ wp-content/themes/virtualops/functions.php
@@ -50,6 +50,7 @@ function virtualops_setup() {
 	register_nav_menus(
 		array(
 			'menu-1' => esc_html__( 'Primary', 'virtualops' ),
+			'menu-2' => esc_html__( 'Footer', 'virtualops' ),
 		)
 	);
 
